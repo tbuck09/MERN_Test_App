2abd3f5448f8e78eb8cb145cf83846ce332f422d
tbuck09 <tylerbuck09@gmail.com>
2019-07-22 12:13:49 -0600

    Added auth functionality (middleware, routes); Started work on Profile

--- routes/api/users.js
+++ routes/api/users.js
@@ -79,7 +79,6 @@ router.post(
         { expiresIn: 360000 },
         (err, token) => {
           if (err) throw err;
-          console.log(token);
           res.json({ token });
         }
       );
